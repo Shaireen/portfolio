b48b65254c99ab8344246f2fd3ff524afc63702d
Marcelina Jankowska <jankowskam401@gmail.com>
2020-10-04 20:13:23 +0200

    responsiveness wip

---
+++ script.js
@@ -0,0 +1,15 @@
+/*window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  window.addEventListener("scroll", checkPosition);
+}
+
+function checkPosition() {
+  let headingPosition = document.querySelector(".mainheading").getBoundingClientRect();
+  const nav = document.querySelector(".nav-wrapper");
+  if (headingPosition.y < -1) {
+    nav.classList.add("bgwhite");
+  } else {
+    nav.classList.remove("bgwhite");
+  }
+} */
