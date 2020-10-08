c6a4d05b97ec8e38f1ca71650293da0462f77941
Marcelina Jankowska <jankowskam401@gmail.com>
2020-10-08 20:15:35 +0200

    navigation working

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-/*window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", start);
+
+let windowHeight;
 
 function start() {
   checkPosition();
+  burgerMenu();
+  window.addEventListener("resize", checkHeight);
 }
 
 function checkPosition() {
-  let activeSection = document.querySelector(".active");
-  console.log("eyy");
-  activeSection.querySelector(".gridtext").classList.add("slide-left");
-  activeSection.querySelector(".projectphoto").classList.add("slide-right");
+  document.querySelectorAll(".section").forEach((section) => {
+    let sectionId = section.getAttribute("id");
+    if (section.getBoundingClientRect().y === 0 && sectionId !== "section0" && sectionId !== "section1") {
+      let gridText = section.querySelector(`#${sectionId} .gridtext`);
+      let photoContainer = section.querySelector(`#${sectionId} .projectphoto`);
+
+      if (gridText.classList.contains("slide-right2") == false) {
+        gridText.classList.add("slide-right2");
+        photoContainer.classList.add("slide-right2");
+      }
+    }
+    if (section.getBoundingClientRect().y === 0 && sectionId == "section1") {
+      const projectsHeading = document.querySelector(".mainheading");
+      if (projectsHeading.classList.contains("slide-in") == false) {
+        projectsHeading.classList.add("slide-in");
+      }
+    }
+  });
+  setTimeout(checkPosition, 10);
 }
-*/
+
+function burgerMenu() {
+  const icon = document.querySelector(".spanwrapper");
+  icon.addEventListener("click", animateSpans);
+
+  const menuLinks = document.querySelectorAll("#menu li");
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      menuLinks.forEach((link) => {
+        if (link.classList.contains("active")) {
+          link.classList.remove("active");
+        }
+      });
+      this.classList.add("active");
+    });
+  });
+}
+
+function animateSpans() {
+  const menu = document.querySelector("#menuToggle");
+  menu.classList.toggle("animatespan");
+}
+
+function checkHeight() {
+  windowHeight = window.innerHeight;
+}
